c910b893781f69eb81cda5338cced9811dd6ba93
ZZ-SEC <105146634+ZZ-SEC@users.noreply.github.com>
2022-08-28 23:35:03 +0800

    Add files via upload

--- val/cat.py
+++ val/cat.py
@@ -1,1 +1,9 @@
-
+import pandas as pd
+import numpy as np
+index_all=[]
+for i in range(0,10):
+    index=pd.read_csv("indexes_val"+str(i)+".csv")
+    index_all.append(index)
+index_all=np.concatenate(index_all,axis=0)
+df=pd.DataFrame(index_all,columns=["a","b","c","d"])
+df.to_csv("index_val.csv",index=False)
